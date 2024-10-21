f5f3360a0f39c87f4147388e3aa80c7ab0d1410d
nurlan <baitassovnurlan1507@gmail.com>
2024-10-21 13:31:52 +0500

    feat: make http listen user routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import {createServer,IncomingMessage,ServerResponse} from 'http'
+import { createServer, IncomingMessage, ServerResponse } from 'http'
+import { parse } from 'url'
+import { usersRouter } from './routes/user'
 
 const PORT = 3000
 
-const server = createServer((req,res)=>{
 
-})
-server.listen(PORT, ()=>{
+const requestListener = (req: IncomingMessage, res: ServerResponse) => {
+    const { url } = req;
+    const parsedUrl = parse(url || '', true);
+    const path = parsedUrl.pathname || '';
+
+
+    if (path.startsWith('/api/users')) {
+        usersRouter(req, res);
+    } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: 'Resource not found' }));
+    }
+};
+
+const server = createServer(requestListener)
+
+server.listen(PORT, () => {
     console.log(
         'Server started on port ' + PORT
     )
